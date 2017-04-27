ce32ffe3a5acadd37e6ebde1b3d4133c67017d04
ronlv10 <ronlv10@gmail.com>
2017-04-27 19:38:11 +0300

    copy from bullshit

--- static/framework/js/config.router.js
+++ static/framework/js/config.router.js
@@ -16,6 +16,18 @@ function ($stateProvider, $urlRouterProvider) {
     $stateProvider.state('home', {
         url: "/home",
         templateUrl: "/static/framework/views/HomeView/home.html"
+    }).state('create-game', {
+        url: "/create-game",
+        templateUrl: "/static/framework/views/CreateGameView/create-game.html"
+    }).state('join-game', {
+        url: "/join-game",
+        templateUrl: "/static/framework/views/JoinGameView/join-game.html"
+    }).state('before-start', {
+        url: "/before-start",
+        templateUrl: "/static/framework/views/BeforeStartView/before-start.html"
+    }).state('show-question', {
+        url: "/show-question",
+        templateUrl: "/static/framework/views/ShowQuestionView/show-question.html"
     });
 
 }]);
