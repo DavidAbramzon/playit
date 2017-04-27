1c91607e9689034f4dd453e57bd6f187516e9dc7
ronlv10 <ronlv10@gmail.com>
2017-04-27 11:37:39 +0300

    finish models

--- playit/models.py
+++ playit/models.py
@@ -1,9 +1,6 @@
 import datetime
 
-from django.contrib.auth.models import User
 from django.db import models
-
-
 
 
 class DefaultModel(models.Model):
@@ -27,8 +24,7 @@ class Game(DefaultModel):
     """
     pin_code = models.IntegerField()
     game_type = models.ForeignKey(GameType)
-    #game_manager = models. """need to hold object of gameManager"""
-# todo: check for the game manager option.
+    game_manager = models.ForeignKey(GameManager)
 
 class GameType(DefaultModel):
 
@@ -36,54 +32,26 @@ class GameType(DefaultModel):
     python_model_name = models.TextField()
 
 
-
-class Work(DefaultModel):
-    """
-    Model for saving movies and episodes .
-     maybe the word Work is not very clear
-    """
-    work_name = models.CharField(max_length=50)
-    work_type = models.TextField(blank=True)  ## for now text 'movie\episode' maybe in future foreign key
-
-    # subtitle_path = models.FileField()
-
-    def __repr__(self):
-        return self.work_name
-
-    def __unicode__(self):
-        return self.work_name
-
-# todo : example delete me
-
-
-
-
-class Subtitle(DefaultModel):
-    """
-    Model for saving subtitle uploaded by the user
-    """
-    subtitle_name = models.CharField(max_length=500)
-    work = models.ForeignKey(Work)  ## link to  the eposide/movie
-    owner = models.ForeignKey(User)
-    subtitle_path = models.FileField(upload_to="subtitles/%Y/%m/%d/")
-
-
 class Answer(DefaultModel):
     player = models.ForeignKey(Player)
-    # answer field will be by user
 
 
 class GameManager(DefaultModel):
 
     def get_next_question(self):
         pass
-    def save_answer(self,player):
+
+    def save_answer(self, player):
         pass
+
     def get_round_summary(self):
         pass
+
     def get_score_board(self):
         pass
+
     def get_answer(self):
         pass
+
     def choose_answer(self):
         pass
